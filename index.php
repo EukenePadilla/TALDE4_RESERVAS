a2739824f540ba4d68fa23983f315d9f4d08c245
ikaslea <ikaslea@ETHAZI16>
2019-10-18 14:39:16 +0200

    Añadir los Scripts correspondientes a su version

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <title>Eco Vehicles</title>
 <link rel="stylesheet" type="text/css" href="view/css/style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css crossorigin="anonymous">
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <!-- //////////////////////EDER//////////////////////////// -->
 		<nav class="navbar navbar-inverse">
