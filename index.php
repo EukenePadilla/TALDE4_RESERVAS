d9ff39f62945501e331201ed7a1ce4747d3a15f7
ikaslea <ikaslea@ETHAZI16>
2019-10-18 14:36:22 +0200

    creacion de carpeta para el CSS, Implementacion del Navbar.

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-esto es un documento
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Eco Vehicles</title>
+<link rel="stylesheet" type="text/css" href="view/css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<!-- //////////////////////EDER//////////////////////////// -->
+		<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+<body>
+<!-- /////////////////////EUKENE//////////////////////////// -->
+
+
+
+
+
+
+<!-- ////////////////////////////XARLES////////////////////////////// -->
+</body>
+
+</html>
