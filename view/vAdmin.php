fdb84078b4a254a073878a3ea650b37a7e67bdfe
ikaslea <jontxust0@gmail.com>
2019-10-21 11:05:04 +0200

    diseño de la vista vAdmin

--- view/vAdmin.php
+++ view/vAdmin.php
@@ -1,13 +1,107 @@
 <html>
-<head>vAdmin</head>
+<head>
+<title>Administracion</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
 <body>
-	<button>DELETE</button>
-	<button>UPDATE</button>
+	<header>
+		<h1>Admin</h1>
+	</header>
 	
-	<button>DELETE</button>
-	<button>UPDATE</button>
-	
-	<button>DELETE</button>
-	<button>UPDATE</button>
+	<div id="paneladmin">
+		<div class="panel" id="panelvehiculos">
+			<h2>Usuarios</h2>
+    		<ul>
+    			<li>
+         			<span>Item 1</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 2</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 3</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+            	<li>
+         			<span>Item 4</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 5</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        	</ul>
+        	<button class="insert">INSERT</button>
+    	</div>
+    	<div class="panel" id="panelreservas">
+    		<h2>Reservas</h2>
+    		<ul>
+ 				<li>
+     				<span>Item 1</span>  	
+            		<button class="delete">DELETE</button>
+            		<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 2</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 3</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 4</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 5</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+    		</ul>
+    		<button class="insert">INSERT</button>
+    	</div>
+    	<div class="panel" id="panelvehiculos">
+    		<h2>Vehiculos</h2>
+    		<ul>
+    			<li>
+         			<span>Item 1</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 2</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 3</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+            	<li>
+         			<span>Item 4</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        		<li>
+         			<span>Item 5</span>  	
+                	<button class="delete">DELETE</button>
+                	<button class="update">UPDATE</button>
+        		</li>
+        	</ul>
+        	<button class="insert">INSERT</button>
+    	</div>
+    </div>
 </body>
 </html>
+
