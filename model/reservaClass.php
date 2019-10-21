07ac215a1c4cae1f4f8b5d05ad0bc62a72716f19
xarles goitiz artola <xarlesgoi97@gmail.com>
2019-10-21 13:10:41 +0200

    reservaModel y reservaClass rellenado.

--- model/reservaClass.php
+++ model/reservaClass.php
@@ -1,1 +1,127 @@
 <?php
+
+class vehiculoClass{
+    protected $idReserva;
+    protected $fechaReserva;
+    protected $fechaReservada;
+    protected $horaInicio;
+    protected $horaFin;
+    protected $idUsuario;
+    protected $idVehiculo;
+    /**
+     * @return mixed
+     */
+    public function getIdReserva()
+    {
+        return $this->idReserva;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaReserva()
+    {
+        return $this->fechaReserva;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaReservada()
+    {
+        return $this->fechaReservada;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHoraInicio()
+    {
+        return $this->horaInicio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHoraFin()
+    {
+        return $this->horaFin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdVehiculo()
+    {
+        return $this->idVehiculo;
+    }
+
+    /**
+     * @param mixed $idReserva
+     */
+    public function setIdReserva($idReserva)
+    {
+        $this->idReserva = $idReserva;
+    }
+
+    /**
+     * @param mixed $fechaReserva
+     */
+    public function setFechaReserva($fechaReserva)
+    {
+        $this->fechaReserva = $fechaReserva;
+    }
+
+    /**
+     * @param mixed $fechaReservada
+     */
+    public function setFechaReservada($fechaReservada)
+    {
+        $this->fechaReservada = $fechaReservada;
+    }
+
+    /**
+     * @param mixed $horaInicio
+     */
+    public function setHoraInicio($horaInicio)
+    {
+        $this->horaInicio = $horaInicio;
+    }
+
+    /**
+     * @param mixed $horaFin
+     */
+    public function setHoraFin($horaFin)
+    {
+        $this->horaFin = $horaFin;
+    }
+
+    /**
+     * @param mixed $idUsuario
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+    }
+
+    /**
+     * @param mixed $idVehiculo
+     */
+    public function setIdVehiculo($idVehiculo)
+    {
+        $this->idVehiculo = $idVehiculo;
+    }
+
+    
+    
+    
+    
+}
--- model/reservaModel.php
+++ model/reservaModel.php
@@ -1,1 +1,97 @@
 <?php
+
+include_once ("connect_data.php");
+include_once ("vehiculoClass.php");
+include_once ("reservaModel.php");
+include_once ("userModel.php");
+
+class vehiculoModel extends vehiculoClass
+{
+    private $link;
+    private $list=array();         //editorial guztien lista - lista de todas editoriales
+    private $listaVehiculos=array(); // editorial honen liburu guztiak - libros de una editorial
+    
+    public function getList() {
+        return $this->list;
+    }
+    
+    public function OpenConnect()
+    {
+        $konDat=new connect_data();
+        try
+        {
+            $this->link=new mysqli($konDat->host,$konDat->userbbdd,$konDat->passbbdd,$konDat->ddbbname);
+            // mysqli klaseko link objetua sortzen da dagokion konexio datuekin
+            // se crea un nuevo objeto llamado link de la clase mysqli con los datos de conexión.
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+        $this->link->set_charset("utf8"); // honek behartu egiten du aplikazio eta
+        //                  //databasearen artean UTF -8 erabiltzera datuak trukatzeko
+    }
+    
+    public function CloseConnect()
+    {
+        mysqli_close ($this->link);
+    }
+    
+    public function findIdReserva($idReserva)
+    {
+        $this->OpenConnect();
+        $sql = "CALL seleccionarTodasReservas($idReserva)";
+        
+        $result = $this->link->query($sql);
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            
+            $nuevoVehiculo->setIdReserva($row['$idReserva']);
+            $nuevoVehiculo->setFechaReserva($row['fechaReserva']);
+            $nuevoVehiculo->setFechaReservada($row['fechaReservada']);
+            $nuevoVehiculo->setHoraInicio($row['horaInicio']);
+            $nuevoVehiculo->setHoraFin($row['horaFin']);
+            $nuevoVehiculo->setIdUsuario($row['idUsuario']);
+            $nuevoVehiculo->setIdVehiculo($row['idVehiculo']);
+            
+            //echo $row['nombreEditorial'];  //ok
+            //  echo $new->getNombreEditorial();  //ok
+        }
+        mysqli_free_result($result);
+        $this->CloseConnect();
+        return $this;
+    }
+    
+    public function setList()
+    {
+        $this->OpenConnect();  // konexioa zabaldu  - abrir conexión
+        $sql = "CALL seleccionarTodasReservas()"; // SQL sententzia - sentencia SQL
+        
+        $result = $this->link->query($sql); // result-en ddbb-ari eskatutako informazio dena gordetzen da
+        // se guarda en result toda la información solicitada a la bbdd
+        
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            
+            $nuevoVehiculo=new self();
+            $nuevoVehiculo->setIdReserva($row['$idReserva']);
+            $nuevoVehiculo->setFechaReserva($row['fechaReserva']);
+            $nuevoVehiculo->setFechaReservada($row['fechaReservada']);
+            $nuevoVehiculo->setHoraInicio($row['horaInicio']);
+            $nuevoVehiculo->setHoraFin($row['horaFin']);
+            $nuevoVehiculo->setIdUsuario($row['idUsuario']);
+            $nuevoVehiculo->setIdVehiculo($row['idVehiculo']);
+            
+            
+            //             $user=new userModel(); //para llamar a la función de libro_model
+            
+            //             $nuevoVehiculo->listaVehiculos=$user->findEditorialBooks($row['idEditorial']);
+            //             // honek itzultzen digu editorial bateko liburu guztien zerrenda
+            
+            array_push($this->list, $nuevaEditorial);
+        }
+        mysqli_free_result($result);
+        //         unset($listaLibrosEditorial);
+        $this->CloseConnect();
+    }
+    
+}
+?>
