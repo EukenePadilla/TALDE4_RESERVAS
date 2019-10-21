8104636a0aa335cb6aa14506fb14f7e70442171b
ikaslea <ikaslea@ETHAZI16>
2019-10-21 11:23:05 +0200

    Corregida la clase vehiculoClass

--- model/vehiculoClass.php
+++ model/vehiculoClass.php
@@ -1,1 +1,108 @@
 <?php
+
+class vehiculoClass{
+    protected $idVehiculo;
+    protected $nombre;
+    protected $modelo;
+    protected $potencia;
+    protected $tipo;
+    protected $img;
+    /**
+     * @return mixed
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * @param mixed $tipo
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdVehiculo()
+    {
+        return $this->idVehiculo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPotencia()
+    {
+        return $this->potencia;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $idVehiculo
+     */
+    public function setIdVehiculo($idVehiculo)
+    {
+        $this->idVehiculo = $idVehiculo;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @param mixed $modelo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    /**
+     * @param mixed $potencia
+     */
+    public function setPotencia($potencia)
+    {
+        $this->potencia = $potencia;
+    }
+
+    /**
+     * @param mixed $img
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+    }
+
+    
+    
+}
