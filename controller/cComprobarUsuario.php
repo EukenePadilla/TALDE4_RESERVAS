15a2fff889100d50362d760597c9418c0e54dfbc
EukenePadilla <tukeplayer@gmail.com>
2019-11-07 01:25:18 +0100

    Intento de añadir con usuario logeado la reserva

---
+++ controller/cComprobarUsuario.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+$user=$_SESSION['usuario'];
+
+echo $user;
+exit();
+?>
--- view/js/script.js
+++ view/js/script.js
@@ -1,122 +1,9 @@
-/*<div class="container-fluid reservas_v_container">
-        <div class="row">
-            <div class="col-12 bg-dark border border-dark rounded-pill">
-              <h2 class="text-center ">BICICLETAS</h2>
-            </div>
-        </div>
-        <div class="row reservas_vehiculos">
 
-
-                <?php               
-                            foreach ($listaVehiculos as $vehiculo)
-                            {       
-                                ?>                              <?php   
-                                if($vehiculo->getTipo()=="Bicicleta"){	?>
-                         	      <div class="card mx-auto col-2" >
-                         	      <img src="<?php echo $vehiculo->getImg();?>" class="card-img-top" alt="<?php echo $vehiculo->getImg();?> ">
-                         		  <div class="card-body">
-                                  <p class="card-text">
-                                  Nombre: <?php echo $vehiculo->getNombre();?> <br>
-                                  Modelo: <?php echo $vehiculo->getModelo();?> <br>
-                                  Potencia: <?php echo $vehiculo->getPotencia();?> <br>
-                                  Tipo: <?php echo $vehiculo->getTipo();?></p>
-                                  </div>
-                                  </div>
-                                  
-                              <?php   
-                            }   	
-                             } ?>
-  </div>
-                             
-<div class="row">
-            <div class="col-12 bg-dark border border-dark rounded-pill">
-              <h2 class="text-center ">MONOPATIN</h2>
-            </div>
-        </div>
-        <div class="row reservas_vehiculos">
-
-
-                <?php               
-                            foreach ($listaVehiculos as $vehiculo)
-                            {       
-                                ?>                              <?php   
-                                if($vehiculo->getTipo()=="Monopatin"){	?>
-                         	      <div class="card mx-auto col-2" >
-                         	      <img src="<?php echo $vehiculo->getImg();?>" class="card-img-top" alt="<?php echo $vehiculo->getImg();?> ">
-                         		  <div class="card-body">
-                                  <p class="card-text">
-                                  Nombre: <?php echo $vehiculo->getNombre();?> <br>
-                                  Modelo: <?php echo $vehiculo->getModelo();?> <br>
-                                  Potencia: <?php echo $vehiculo->getPotencia();?> <br>
-                                  Tipo: <?php echo $vehiculo->getTipo();?></p>
-                                  </div>
-                                  </div>
-                                  
-                              <?php   
-                            }   	
-                             } ?>
-        </div>
-<div class="row">
-            <div class="col-12 bg-dark border border-dark rounded-pill">
-              <h2 class="text-center ">PATINETE</h2>
-            </div>
-        </div>
-        <div class="row reservas_vehiculos">
-
-
-                <?php               
-                            foreach ($listaVehiculos as $vehiculo)
-                            {       
-                                ?>                              <?php   
-                                if($vehiculo->getTipo()=="Patinete"){	?>
-                         	      <div class="card mx-auto col-2" >
-                         	      <img src="<?php echo $vehiculo->getImg();?>" class="card-img-top" alt="<?php echo $vehiculo->getImg();?> ">
-                         		  <div class="card-body">
-                                  <p class="card-text">
-                                  Nombre: <?php echo $vehiculo->getNombre();?> <br>
-                                  Modelo: <?php echo $vehiculo->getModelo();?> <br>
-                                  Potencia: <?php echo $vehiculo->getPotencia();?> <br>
-                                  Tipo: <?php echo $vehiculo->getTipo();?></p>
-                                  </div>
-                                  </div>
-                              <?php   
-                            }   	
-                             } ?>
-        </div>        
-<div class="row">
-            <div class="col-12 bg-dark border border-dark rounded-pill">
-              <h2 class="text-center ">COCHE</h2>
-            </div>
-        </div>
-        <div class="row reservas_vehiculos">
-
-
-                <?php               
-                            foreach ($listaVehiculos as $vehiculo)
-                            {       
-                                ?>                              <?php   
-                                if($vehiculo->getTipo()=="coche"){	?>
-                         	      <div class="card mx-auto col-2" >
-                         	      <img src="<?php echo $vehiculo->getImg();?>" class="card-img-top" alt="<?php echo $vehiculo->getImg();?> ">
-                         		  <div class="card-body">
-                                  <p class="card-text">
-                                  Nombre: <?php echo $vehiculo->getNombre();?> <br>
-                                  Modelo: <?php echo $vehiculo->getModelo();?> <br>
-                                  Potencia: <?php echo $vehiculo->getPotencia();?> <br>
-                                  Tipo: <?php echo $vehiculo->getTipo();?></p>
-                                  </div>
-                                  </div>
-                              <?php   
-                            }   	
-                             } ?>
-        </div>        
-        </div>
-
-*/
 var midato= new Object();
 var usuario = {};
 var userdata = {};
 var tipos = [];
+var $idUsuario;
 var tp=-1;// para guardar el tipo de usuario en numero(1==admin, 2==usuario normal)
 var m;//variable utilizada en frontal para sacar los tipos
 var idVehiculo;// variable utilizada para coger la idVehiculo en reserva.
@@ -381,12 +268,29 @@ function iniciar_reservas(){
                	$fReservada =$("#reservaInputreservada").val();
                	$rango =$("#reservaInputRango").val();
                	
-               	var $idUsuario = 1;
-               	alert(idVehiculo + " -> idvehiculo ||" + 
-               			$idUsuario + " ->  idUsuario  || " +
-               			$fReserva +" ->  reserva || " + 
-               			$fReservada +" ->  reservada  || " + 
-               			$rango+ " ->  rango || ");
+               	$.ajax({
+           			
+           		 	type:"GET",
+           	        url:"controller/cComprobarUsuario.php",
+           	        success: function($idUsuario){
+           	        	
+           		 		alert($idUsuario);
+
+           	        },
+           	        error: function(xhr){
+           	            alert("An error occured: "+xhr.status+" "+xhr.statusText);
+           	        }
+
+
+
+           	});
+               	
+               	
+//               	alert(idVehiculo + " -> idvehiculo ||" + 
+//               			$idUsuario + " ->  idUsuario  || " +
+//               			$fReserva +" ->  reserva || " + 
+//               			$fReservada +" ->  reservada  || " + 
+//               			$rango+ " ->  rango || ");
 //               	alert($idUsuario + " idusuario");
                	$.ajax({
            			
